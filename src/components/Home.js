2a7c8cc583f0bcfe4aaee35cc37942cc3d3ba509
auma2022 <auma.amwayi@student.moringaschool.com>
2022-07-20 11:57:13 +0300

    add comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,8 @@
+/*
+ Importing name and city from the data.js file
+2. Storing the variables returned from the data.js file with the React component
+3. Rendering the returned variables to the screen.
+*/ 
 import React from "react";
 import { name, city } from "../data/data.js";
 
